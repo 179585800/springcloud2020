1a0feada2bb43ec5fef9189674f56293bdec14f2
wangxiaowei <xwei_wang@126.com>
2020-05-27 14:24:41 +0800

    tijiao

---
+++ cloud-provide-payment8001/src/main/java/com/wxw/springcloud/PaymentMain8001.java
@@ -0,0 +1,22 @@
+package com.wxw.springcloud;
+
+import org.hibernate.validator.internal.metadata.aggregated.rule.ReturnValueMayOnlyBeMarkedOnceAsCascadedPerHierarchyLine;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version v1.0
+ * @ProjectName: cloud2020
+ * @ClassName: PaymentMain8001
+ * @Description: TODO(一句话描述该类的功能)
+ * @Author: wangxw
+ * @Date: 2020/5/27 14:12
+ */
+@SpringBootApplication
+public class PaymentMain8001
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
